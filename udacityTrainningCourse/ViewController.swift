b01bf1047f5aa7d55488de158a914892ef0e045c
Eslam Mohamed Moemen <eslammoemen.headway.contact@gmail.com>
2019-04-26 01:14:20 +0200

    master commit

--- udacityTrainningCourse/ViewController.swift
+++ udacityTrainningCourse/ViewController.swift
@@ -2,24 +2,56 @@
 //  ViewController.swift
 //  udacityTrainningCourse
 //
-//  Created by Eslam Moemen on 5/5/18.
+//  Created by Eslam Moemen on 6/3/18.
 //  Copyright © 2018 udacity. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITextFieldDelegate {
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var textinput: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopButton.isEnabled = false
+        textinput.delegate = self
+        textView.isEditable = false
+       
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //hide keyboard when touching anywhere esle but textinput
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+     // hide keyboard when hitting return
+        
+        // textinput.resignFirstResponder()
+        textView.text = textinput.text
+        return true
+    }
+  
+    
+    @IBAction func recordAction(_ sender: Any) {
+        recordButton.isEnabled = false
+        stopButton.isEnabled = true
+        statusLabel.text = "Recording"
+    }
+   
+    @IBAction func stopAction(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+        statusLabel.text = "Stopped"
+    }
+    
+    
+    
 
 }
 
